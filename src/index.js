71be997568961b1391e291298cb79ff4c5ae23eb
Igor Stanevoy <igor.stanevoj@gmail.com>
2020-10-03 23:43:59 +0300

    feat: solve array tasks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+    if (array.length === 0 || arguments.length === 0) {
+      return 0;
+    }
+
+    let minValue = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < minValue) {
+          minValue = array[i];
+      }
+    }
+
+    return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+    if (array.length === 0 || arguments.length === 0) {
+      return 0;
+    }
+
+    let maxValue = array[0];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > maxValue) {
+          maxValue = array[i];
+      }
+    }
+
+    return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+    if (array.length === 0 || arguments.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    let count = 0;
+
+   for (let i = 0; i < array.length; i++) {
+     sum += array[i];
+     count += 1;
+   }
+
+   const result = sum / count;
+   return result;
 }
